9248781b61908a8073adf878620af18c09e4614d
Alexander Skladanek <ajskladanek@icloud.com>
2021-03-19 00:57:25 -0400

    Finished adding default events and reminders, going to continue working on adding buttons to the events and reminders and getting the view frames to update properly

--- ScheDuel/HomeSafe.swift
+++ ScheDuel/HomeSafe.swift
@@ -8,6 +8,11 @@
 import UIKit
 
 class HomeSafe: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.clipsToBounds = true
+        update()
+    }
     @objc func update(){
         var containerStack : [UICustomObject] = []
         //Defining my own vertical stack for the home screen as an array of stuff
@@ -30,55 +35,74 @@ class HomeSafe: UIViewController {
         containerStack.append(whatsNextLabel)//5+reminders
         let defaultCalEvent = calEvent(text: "test", time: 0, duration: 1, date: 0)
         eventList.append(defaultCalEvent)
-        let defaultCalEventUI = UICustomObject(element: 3, text: defaultReminder.text, pos: 0)
+        let defaultCalEventUI = UICustomObject(element: 2, text: defaultReminder.text, pos: 0)
         containerStack.append(defaultCalEventUI)
         //Everything above is adding elements to my array for my vertical stack
         //Now i will turn them into real objects and display them!
-        let height = view.bounds.height
-        let width = view.bounds.width
+        let height = self.view.bounds.height
+        //These values are incorrect, not sure why
+        let width = self.view.frame.width
+        
+        
         let elements = containerStack.count
         let seperation = height / CGFloat(elements)
+        let fullFrame = CGRect(x: 0, y: 0, width: width, height: seperation)
         var i = 0
-        print("passed line 42")
         //Remove all previous subviews
         while (i < containerStack.count){
             switch (containerStack[i].UIElement){
             case 0://label
                 let label = UILabel()
-                label.frame = CGRect(x: 0, y: CGFloat(i) * seperation,width: width, height: 0.75*seperation)
+                label.frame = fullFrame
+                label.frame.origin.y += (CGFloat(i) * seperation)
                 label.text = containerStack[i].text
                 self.view.addSubview(label)
-                print("added subview")
                 break;
             case 1://bar
-                let bar = UIProgressView(frame: CGRect(x: 0, y: CGFloat(i) * seperation,width: width, height: 0.75*seperation))
+                let bar = UIProgressView(frame: fullFrame)
+                bar.frame = fullFrame
+                bar.frame.origin.y += (CGFloat(i) * seperation) + seperation/2.0
                 bar.progress = 0
                 //add time function later
                 self.view.addSubview(bar)
                 break;
             case 2://event
-                //To be implemented
+                let button = UIButton(frame: CGRect(x: 0, y: CGFloat(i) * seperation, width:(0.5 * seperation), height: seperation) )
+                button.setImage( UIImage.init(systemName: "squareshape"), for: UIControl.State.normal)
+                button.setImage( UIImage.init(systemName: "checkmark.square"), for: UIControl.State.highlighted)
+                self.view.addSubview(button)
+                let label = UILabel()
+                label.frame = fullFrame
+                label.frame.origin.y += (CGFloat(i) * seperation)
+                label.frame.origin.x += seperation/2.0
+                label.text = String(eventList[containerStack[i].objPos].time) + " " + eventList[containerStack[i].objPos].text
+                self.view.addSubview(label)
                 break;
             case 3://reminder
-                //To be implemented
+                let button = UIButton(frame: CGRect(x: 0, y: CGFloat(i) * seperation, width:(0.5 * seperation), height: seperation) )
+                button.setImage( UIImage.init(systemName: "circle"), for: UIControl.State.normal)
+                button.setImage( UIImage.init(systemName: "record.circle"), for: UIControl.State.highlighted)
+                self.view.addSubview(button)
+                let label = UILabel()
+                label.frame = fullFrame
+                label.frame.origin.y += (CGFloat(i) * seperation)
+                label.frame.origin.x += seperation/2.0
+                label.text = String(reminderList[containerStack[i].objPos].time) + " " + reminderList[containerStack[i].objPos].text
+                self.view.addSubview(label)
                 break;
             case 4://title
                 let label = UILabel()
-                label.frame = CGRect(x: 0, y: CGFloat(i) * seperation,width: width, height: 0.75*seperation)
+                label.frame = fullFrame
+                label.frame.origin.y += (CGFloat(i) * seperation)
                 label.text = containerStack[i].text
                 label.font = label.font.withSize(CGFloat(label.font.pointSize + 2.0))
                 label.textAlignment = NSTextAlignment.center
                 self.view.addSubview(label)
-                print("added subview")
                 break;
             default://error?
                 break;
             }
             i = i + 1
         }
-    }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        update()
     }
 }
